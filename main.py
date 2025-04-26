b3e069f2760c89981c798f7d722d5b86ec113814
jhernandez4 <jokhernandez120744@gmail.com>
2025-04-25 20:18:39 -0700

    fix(main.py): allow frontend to call backend api

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import (
     create_db_and_tables, insert_users_to_db, install_fuzzy_search_extension
 )
 from .routers import users
 
 app = FastAPI()
+
+# Origins allowed to call this backend API
+origins = [
+    # Local frontend URL 
+    "http://localhost:5173/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 
